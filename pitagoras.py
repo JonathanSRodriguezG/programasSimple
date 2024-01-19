fc17c6b455ce2ac77c30e47758860eecbaab179f
JonathanSRodriguezG <156339537+JonathanSRodriguezG@users.noreply.github.com>
2024-01-19 11:49:53 -0500

    Solucion del ejercicio de pitagoras

---
+++ pitagoras.py
@@ -0,0 +1,4 @@
+cateto1 = float(input("Ingrese la longitud del cateto 1:  "))
+cateto2 = float(input("Ingrese la longitud del cateto 2:  "))
+hipotenusa =  (cateto1**2 + cateto2**2)**0.5
+print(f"La hipotenusa es: {hipotenusa}")
